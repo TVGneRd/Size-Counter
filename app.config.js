2f5a4e8092e811ad1b6d7cd0add43f3226b9139c
Дима <43295090+TVGneRd@users.noreply.github.com>
2022-05-10 14:58:18 +0200

    Добавлен шаблон app.config.js

---
+++ app.config.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+export const firebaseConfig = {
+	apiKey: "apiKey",
+	authDomain: "sizecounter.firebaseapp.com",
+	databaseURL: "https://sizecounter-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "sizecounter",
+	storageBucket: "sizecounter.appspot.com",
+	messagingSenderId: "137098275366",
+	appId: "appId",
+	measurementId: "measurementId"
+};
